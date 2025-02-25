20dd2de41b429c13f6c11afd152bf33f98982de9
mdabdulkyum1 <kyummdabdul@gmail.com>
2025-02-25 12:25:21 +0600

    update url

--- src/hooks/AxiosPublic/useAxiosPublic.jsx
+++ src/hooks/AxiosPublic/useAxiosPublic.jsx
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000'
-    // baseURL: ''
+    // baseURL: 'http://localhost:5000'
+    baseURL: 'https://pay-nova-server-production.up.railway.app'
   });
 
   
